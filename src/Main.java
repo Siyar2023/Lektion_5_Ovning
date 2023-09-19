062f80ce412e5a617adbfa46d31539190f9470c8
Roni7823 <siy_sar78@yahoo.se>
2023-09-19 15:30:39 +0200

    initial Commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+
+public class Student {
+    public static void main(String[] args) {
+        Scanner ab = new Scanner(System.in);
+        Student jonas = new Student();
+        System.out.println("jonas");
+        }
+    }
