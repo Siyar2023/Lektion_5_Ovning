db02188dac8ed79eb9b323590e26e4fd2f651b9f
Roni7823 <siy_sar78@yahoo.se>
2023-09-21 19:45:21 +0200

    hej

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,47 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner name = new Scanner(System.in);
-        Student jonas = new Student();
-        System.out.println("jonas");
-    }
+        Scanner myScanner = new Scanner(System.in); // Skapa en scanner-instans
+
+        // Fråga efter namn
+        System.out.print("Ange ditt namn: ");
+        String namn = myScanner.nextLine();
+        System.out.println("Player #1: " + namn);
+
+        // Fråga efter ett heltal X
+        System.out.print("Ange ett heltal X: ");
+        int X = myScanner.nextInt();
+        myScanner.nextLine(); // Läs upp överbliven rad
+
+        // Loop för att visa heltalen från 1 till X
+        for (int i = 1; i <= X; i++) {
+            System.out.println(i); // Ta bort onödig + före i
+        }
+
+
+        // do-while loop
+        /* do {
+            System.out.print("Mata in något: ");
+            String input = myScanner.nextLine();
+
+            System.out.println("Du matade in: " + input);
+        } while (true); // Villkor som alltid är sant */
+
+
+
+        // Låt användaren mata in något tills man skriver "stop"
+        boolean isPlaying = true; // boolean-variabel skapad
+        do {
+            System.out.print("Mata in något eller skriv 'stop' för att avsluta programmet: ");
+            String input = myScanner.nextLine();
+
+            if (input.equals("stop")) {
+                isPlaying = false; // om användaren matar in "stop"
+            } else {
+                System.out.println("Du matade in: " + input);
+            }
+        } while (isPlaying); // Loop avslutas när isPlaying är false
 
     }
+}
+
